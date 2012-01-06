7b1845e9b5ddfc497db1d7ed0900a172dfc745a4
Taddeus Kroes <taddeuskroes@hotmail.com>
2012-01-06 20:06:43 +0100

    Source code cleanup.

--- problem81.py
+++ problem81.py
@@ -1,13 +1,6 @@
 from numpy import array, zeros
 
-#m = array([[131, 673, 234, 103, 18 ],
-#           [201, 96 , 342, 965, 150],
-#           [630, 803, 746, 422, 111],
-#           [537, 699, 497, 121, 956],
-#           [805, 732, 524, 37 , 331]])
-f = open('matrix.txt', 'r')
-m = array([map(int, l.split(',')) for l in f.readlines()])
-f.close()
+m = array([map(int, l.split(',')) for l in open('matrix.txt', 'r').readlines()])
 
 def add(x, y):
     global m
