d8321647bfb90a6711ed8f478754bb06bd3e4fa3
Taddeus Kroes <taddeuskroes@hotmail.com>
2012-12-01 13:19:34 +0100

    Solved 34

---
+++ problem34.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from itertools import combinations
+from math import factorial
+
+def digits(n):
+    return map(int, str(n))
+
+def facsum(n):
+    return sum(facs[d] for d in digits(n))
+
+facs = map(factorial, range(10))
+s = 0
+
+for n in xrange(10, 99999):
+    if n == facsum(n):
+        s += n
+
+print s
