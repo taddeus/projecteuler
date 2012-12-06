76268ce5b8fd9ca51223623049a64d352a409e6f
Taddeus Kroes <taddeuskroes@gmail.com>
2012-12-06 15:06:53 +0100

    Solved 45

---
+++ problem45.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+def tri(n): return n * (n + 1) / 2
+def pent(n): return n * (3 * n - 1) / 2
+def hexa(n): return n * (2 * n - 1)
+funcs = (tri, pent, hexa)
+
+n = [20] * 3
+vals = [f(20) for f in funcs]
+
+while len(set(vals)) > 1 or vals[0] == 40755:
+    mini = 0
+    minv = None
+
+    for i, v in enumerate(vals):
+        if minv is None or v < minv:
+            mini = i
+            minv = v
+
+    n[mini] += 1
+    vals[mini] = funcs[mini](n[mini])
+
+print vals[0]
