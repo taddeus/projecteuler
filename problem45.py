46eb33ea189c60a5808c9bf2f80265a275444a06
Taddeus Kroes <taddeuskroes@hotmail.com>
2012-12-14 16:55:49 +0100

    Code cleanup

--- problem45.py
+++ problem45.py
@@ -3,9 +3,9 @@ def tri(n): return n * (n + 1) / 2
 def pent(n): return n * (3 * n - 1) / 2
 def hexa(n): return n * (2 * n - 1)
 funcs = (tri, pent, hexa)
-
-n = [20] * 3
-vals = [f(20) for f in funcs]
+init = 20
+n = [init] * len(funcs)
+vals = [f(init) for f in funcs]
 
 while len(set(vals)) > 1 or vals[0] == 40755:
     mini = 0
